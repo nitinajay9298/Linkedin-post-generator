3a5fc43265068c3f4c55f7c4c2e4296cff98ae5e
Nitin Jangir <117674116+nitinajay9298@users.noreply.github.com>
2023-06-19 22:26:28 +0530

    Add files via upload

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+interface Validation {
+  type: "positive" | "negative";
+  message: string;
+}
+
+interface RankResponse {
+  score: number;
+  validations: Array<ProcessedValidation>;
+}
+
+interface Rank {
+  score: number;
+  message?: string;
+  type?: "positive" | "negative";
+}
+
+interface AnalysisResult {
+  score: number;
+  comparative: number;
+  calculation: Array<{
+    [token: string]: number;
+  }>;
+  tokens: string[];
+  words: string[];
+  positive: string[];
+  negative: string[];
+}
+interface PostData {
+  post: string;
+  originalPost: string;
+  sentiment: AnalysisResult;
+  postMedia: boolean;
+}
